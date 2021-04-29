c47d5129c4ce149e77959372217c155c5e1f69eb
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2021-04-29 17:28:35 -0500

    Added animation of linearization

---
+++ introduction/slides/auto/modelacion-intro.el
@@ -0,0 +1,37 @@
+(TeX-add-style-hook
+ "modelacion-intro"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "dvisvgm" "hypertex" "aspectratio=169")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("animate" "final") ("zref" "totpages")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "animate"
+    "ifthen"
+    "zref"
+    "atbegshi"
+    "fontawesome"
+    "tikz"
+    "pgfplots"
+    "pgfplotstable"
+    "amsmath"
+    "ccicons")
+   (TeX-add-symbols
+    '("hummeranimation" 1)
+    "velocity"
+    "startbrake"
+    "braketimeconst"))
+ :latex)
+
